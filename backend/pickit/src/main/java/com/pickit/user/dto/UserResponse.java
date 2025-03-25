c4d895348668cd55c8feea4c083919c4042f226e
Yujin Jo <ska05142@naver.com>
2025-03-25 16:58:08 +0900

    feat: user dto folder create

---
+++ backend/pickit/src/main/java/com/pickit/user/dto/UserResponse.java
@@ -0,0 +1,17 @@
+package com.pickit.user.dto;
+
+import com.pickit.global.common.Role;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserResponse {
+    private Long id;
+    private String email;
+    private String name;
+    private String nickname;
+    private String phoneNumber;
+    private String profileImageUrl;
+    private Role role;
+}
