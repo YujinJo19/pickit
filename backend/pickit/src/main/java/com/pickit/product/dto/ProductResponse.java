3276a3122dad11266973c8e7ce9def2d78c20589
Yujin Jo <ska05142@naver.com>
2025-08-16 22:08:46 +0900

    feat: product entity add

---
+++ backend/pickit/src/main/java/com/pickit/product/dto/ProductResponse.java
@@ -0,0 +1,15 @@
+package com.pickit.product.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private Integer price;
+    private String description;
+}
