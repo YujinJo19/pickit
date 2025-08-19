ab554b126bf1e2348646ef8c62cfa0e06c8f2c35
Yujin Jo <ska05142@naver.com>
2025-08-19 15:36:22 +0900

    feat: product, productImage, inventory, category entity

---
+++ backend/pickit/src/main/java/com/pickit/product/entity/Category.java
@@ -0,0 +1,29 @@
+package com.pickit.product.entity;
+
+import com.pickit.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    @Builder.Default
+    private List<Product> products = new ArrayList<>();
+}
