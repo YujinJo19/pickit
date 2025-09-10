c00dddd36e617127125b6b8497329add6471740d
Yujin Jo <ska05142@naver.com>
2025-09-10 17:24:36 +0900

    feat: s3 config setting

---
+++ backend/pickit/src/main/java/com/pickit/config/S3Config.java
@@ -0,0 +1,37 @@
+package com.pickit.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Primary
+    @Bean
+    public S3Client s3Client() {
+        System.out.println("AWS Access Key: " + accessKey);
+        System.out.println("AWS Secret Key: " + secretKey);
+        System.out.println("AWS Region: " + region);
+
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .region(Region.of(region))
+                .build();
+    }
+}
